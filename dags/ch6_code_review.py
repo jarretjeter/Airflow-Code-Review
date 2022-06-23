5aacc6d3d3e910aa09c9c43e04b8717619e87319
Jarret Jeter <jarretjeter@yahoo.com>
2022-06-22 22:47:22 -0700

    refactored code for dag layout

--- dags/ch6_code_review.py
+++ dags/ch6_code_review.py
@@ -8,6 +8,31 @@ import time
 APPLES = ["pink lady", "jazz", "orange pippin", "granny smith", "red delicious", "gala", "honeycrisp", "mcintosh", "fuji"]
 
 
+echo_to_file = BashOperator(
+task_id="echo_to_file",
+# NAME="Jarret",
+bash_command="echo Jarret > /home/jarret/data-engineering-bootcamp/workspace/airflow-code-review/dags/ch6_code_review.txt"
+)
+
+
+@task
+def print_hello():
+    with open("/home/jarret/data-engineering-bootcamp/workspace/airflow-code-review/dags/ch6_code_review.txt", "r") as file:
+        text = file.read()
+        print(f"Hello, {text}!")
+
+
+echo_msg = BashOperator(
+task_id="echo_msg",
+bash_command="echo picking three random apples"
+)
+
+# for task in range(4, 7):
+# @task
+# def rand_apple_task4(message:str) -> str:
+#     for apple in range(len(APPLES) -1):
+        
+
 @dag(
     schedule_interval="@once",
     start_date=datetime.utcnow(),
@@ -18,23 +43,13 @@ APPLES = ["pink lady", "jazz", "orange pippin", "granny smith", "red delicious",
 )
 def ch6_code_review_taskflow():
 
-    echo_to_file = BashOperator(
-    task_id="echo_to_file",
-    # NAME="Jarret",
-    bash_command="echo Jarret > /home/jarret/data-engineering-bootcamp/workspace/airflow-code-review/dags/ch6_code_review.txt"
-    )
+    t1 = echo_to_file
+    t2 = print_hello()
+    t3 = echo_msg
 
-    @task
-    def print_hello():
-        with open("/home/jarret/data-engineering-bootcamp/workspace/airflow-code-review/dags/ch6_code_review.txt", "r") as file:
-            text = file.read()
-            print(f"Hello, {text}!")
 
-    echo_msg = BashOperator(
-    task_id="echo_msg",
-    bash_command="echo picking three random apples"
-    )
 
-    echo_to_file >> print_hello() >> echo_msg
+    t1 >> t2 >> t3
+
 
 dag = ch6_code_review_taskflow()
